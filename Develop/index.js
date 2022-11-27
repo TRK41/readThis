c73660c35ba9c3cb6c9ae0258f9e72204c8dbd52
TRK41 <Troyk@bell.net>
2022-11-27 15:13:14 -0500

    working on index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
+ const questions = ([
+    {
+        type: 'input',
+        message: 'What is you GitHub username',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have ?',
+        name: 'licences',
+        choices :['MIT','APACHE 2.0','GPL 3.0','BSD 3','None']
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'dependencies',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to tests?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'instructions',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contribute',
+    },
+])
+inquirer
+    .prompt(questions)
+// .then(console.log(questions));
+ 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const filename = `${data.title.toLowerCase().split('').join('')}.md`;
 
-// TODO: Create a function to initialize app
-function init() {}
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+          err ? console.log(err) : console.log('Success!')
+      )
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
